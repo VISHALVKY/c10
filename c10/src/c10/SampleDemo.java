b69dbb2f8567399658792b7638811439cb74d812
Srinivasanrbz <srinivasanrbz@gmail.com>
2023-04-28 20:28:59 +0530

    first commit

---
+++ c10/src/c10/SampleDemo.java
@@ -0,0 +1,17 @@
+package c10;
+import java.util.*;
+
+public class SampleDemo {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter the value of variable a");
+		a=s.nextInt();
+		System.out.println("Enter the value of variable b");
+		b=s.nextInt();
+		c=a+b;
+		System.out.println("Sum is "+c);
+	}
+
+}
